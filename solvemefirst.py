fa43125079861116ef6be94da4a15561b8db9277
Kathrin Travers <kathrin.travers@gmail.com>
2019-12-31 20:48:43 +0100

    first task

---
+++ solvemefirst.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def solveMeFirst(a, b):
+    print(__name__)
+    return a+b
+
+
+def main():
+    num1 = int(input('put in the first number: '))
+    num2 = int(input('put in the second number: '))
+
+    print(solveMeFirst(num1, num2))
+
+main()
+# res = solveMeFirst(num1,num2)
+# print(res)
