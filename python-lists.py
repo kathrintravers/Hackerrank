65797c2793fa138dfb94f3a978eaf363dce5fbe2
Kathrin Travers <kathrin.travers@gmail.com>
2020-01-02 21:27:10 +0100

    Add new tasks

---
+++ python-lists.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    N = int(input())
+
+    commands = []
+    my_list = []
+
+    for index in range(0, N):
+        commands.insert(index, input().split(' '))
+
+    for command in commands:
+        if command[0] == 'insert':
+            my_list.insert(int(command[1]), int(command[2]))
+
+        elif command[0] == 'print':
+            print(my_list)
+
+        elif command[0] == 'remove':
+            my_list.remove(int(command[1]))
+
+        elif command[0] == 'append':
+            my_list.append(int(command[1]))
+
+        elif command[0] == 'sort':
+            my_list.sort()
+
+        elif command[0] == 'pop':
+            my_list.pop()
+
+        elif command[0] == 'reverse':
+            my_list.reverse()
+
+        
